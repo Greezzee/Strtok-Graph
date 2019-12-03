8737846e45ebbf10996043e0d25fd354f8614ebd
Евгений Зорин <zorin.eugene@gmail.com>
2019-12-03 19:56:12 +0600

    Graph rework

---
+++ Graph/Graph/GraphDrawer.h
@@ -0,0 +1,79 @@
+#pragma once
+#include "TXLib.h"
+
+typedef double (*GraphFunc) (double);
+
+class GraphDrawer
+{
+public:
+
+	~GraphDrawer() 
+	{
+		delete[] Funcs;
+	}
+
+	GraphDrawer(GraphFunc funcs[], int graphs_count) 
+	{
+		Funcs = new GraphFunc[graphs_count];
+		for (int i = 0; i < graphs_count; i++)
+			Funcs[i] = funcs[i];
+		scale_x = 1;
+		scale_y = 1;
+		graph_count = graphs_count;
+		txCreateWindow(600, 600);
+	}
+
+	void Draw() 
+	{
+		txLine(0, 300, 600, 300);
+		txLine(300, 0, 300, 600);
+
+		for (int i = 0; i < graph_count; i++) {
+			DrawOneGraph(Funcs[i]);
+		}
+	}
+
+	//by default "1" is 100 pixels;
+	void SetScale(double new_x, double new_y) 
+	{
+		scale_x = new_x;
+		scale_y = new_y;
+	}
+
+private:
+
+	void DrawOneGraph(GraphFunc F)
+	{
+		double prev_x, prev_y;
+		prev_x = -300.f / scale_x;
+		prev_y = -F(prev_x);
+
+		for (double x = -300.f / scale_x; x < 300.f / scale_x; x += 1.f / scale_x) {
+			double y = -F(x);
+
+			double realX1 = prev_x * scale_x + 300;
+			double realX2 = x * scale_x + 300;
+			double realY1 = prev_y * scale_y + 300;
+			double realY2 = y * scale_y + 300;
+
+			if (realX1 < 0)   realX1 = 0;
+			if (realX2 < 0)   realX2 = 0;
+			if (realY1 < 0)   realY1 = 0;
+			if (realY2 < 0)   realY2 = 0;
+
+			if (realX1 > 600) realX1 = 600;
+			if (realX2 > 600) realX2 = 600;
+			if (realY1 > 600) realY1 = 600;
+			if (realY2 > 600) realY2 = 600;
+
+			txLine(realX1, realY1, realX2, realY2);
+
+			prev_x = x;
+			prev_y = y;
+		}
+	}
+
+	GraphFunc* Funcs;
+	double scale_x, scale_y;
+	size_t graph_count;
+};
--- Graph/Graph/Source.cpp
+++ Graph/Graph/Source.cpp
@@ -1,46 +1,31 @@
 #include <stdlib.h>
 #include <math.h>
-#include "TXLib.h"
+#include "GraphDrawer.h"
 
-const int SCREEN_X = 800, SCREEN_Y = 600;
-typedef double (*PlotFunc)(double);
-void drawPlot(PlotFunc func, double scaleX, double scaleY) {
-	txLine(0, 300, 800, 300);
-	txLine(400, 0, 400, 600);
-
-	static char matrix[SCREEN_Y][SCREEN_X] = {};
-	for (int x = 0; x < SCREEN_X; x++) {
-		double y = -func((double)(x - SCREEN_X / 2) * scaleX) * scaleY + SCREEN_Y / 2;
-		if (y != NAN && y >= 0 && y < SCREEN_Y)
-			matrix[(int)y][x] = 1;
-	}
-	for (int y = 0; y < SCREEN_Y; y++)
-		for (int x = 0; x < SCREEN_X; x++)
-			if (matrix[y][x])
-				txPixel(x, y, 1, 1, 1);
-
-}
-
-void drawMultiplyPlots(PlotFunc plots[], size_t count, double scaleX, double scaleY) {
-	for (int i = 0; i < count; i++)
-		drawPlot(plots[i], scaleX, scaleY);
-}
+typedef double (*GraphFunc) (double);
 
 double F(double x) {
-	return x;
+	return x * x - 1;
 }
+
 double G(double x) {
-	return 0;
+	if (x == 0)
+		return 0;
+	return 1 / x;
 }
 
 double H(double x) {
-	return 0;
+
+	return sin(x);
 }
 
 int main() {
-	txCreateWindow(SCREEN_X, SCREEN_Y);
-	PlotFunc plots[] = { F, G, H };
-	drawMultiplyPlots(plots, 3, 100, 100);
+
+	GraphFunc graphs[] = { F, G, H };
+
+	GraphDrawer g(graphs, 3);
+	g.SetScale(100, 100);
+	g.Draw();
 	return 0;
 }
 
